0bb5c8bb01198124e6656ba3dfc801fa11700069
vickieeeeee <okonesther167@gmail.com>
2023-04-23 13:53:39 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main-Entrypoint 
+ * main - Entry point 
  *
  * Return: Always 0 (Success)
  */
@@ -13,4 +13,3 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
-	
